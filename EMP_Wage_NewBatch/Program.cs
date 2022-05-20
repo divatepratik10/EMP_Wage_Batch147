cf55857a169c792dcead176ce9e8d71ff8545d58
divatepratik10 <pratikrdivate3210@gmail.com>
2022-05-21 01:20:17 +0530

    [Pratik] added : Employee Attendance Programme

--- EMP_Wage_NewBatch/Program.cs
+++ EMP_Wage_NewBatch/Program.cs
@@ -10,6 +10,20 @@ namespace EMP_Wage_NewBatch
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Employee Wage Programme.");
+            Random random = new Random();
+            const int EMP_PRESENT = 1;
+            int empinput = random.Next(0,2);
+
+            if (empinput == EMP_PRESENT)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+                Console.WriteLine("Employee is Absent");
+
+            Console.ReadLine();
+
         }
     }
 }
