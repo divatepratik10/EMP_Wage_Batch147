6b7d0eca8cd40f400d4a4e260c744819784b64fb
divatepratik10 <pratikrdivate3210@gmail.com>
2022-05-21 01:26:47 +0530

    [Pratik] added : Employee Daily Wage Programme

--- EMP_Wage_NewBatch/Program.cs
+++ EMP_Wage_NewBatch/Program.cs
@@ -14,14 +14,21 @@ namespace EMP_Wage_NewBatch
             Random random = new Random();
             const int EMP_PRESENT = 1;
             int empinput = random.Next(0,2);
-
+            int WageperHour = 20;
+            int DailyHours;
+            int DailyWage;
             if (empinput == EMP_PRESENT)
             {
                 Console.WriteLine("Employee is Present");
+                DailyHours = 8;
             }
             else
+            {
                 Console.WriteLine("Employee is Absent");
-
+                DailyHours = 0;
+            }
+            DailyWage = DailyHours * WageperHour;
+            Console.WriteLine("Daily Wage of Employee is {0}", DailyWage);
             Console.ReadLine();
 
         }
