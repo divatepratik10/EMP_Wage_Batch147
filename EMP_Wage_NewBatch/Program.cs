70b20970af682bf8d5fec867c5f4d64c50f15520
divatepratik10 <pratikrdivate3210@gmail.com>
2022-05-21 01:55:59 +0530

    [Pratik] added : Monthly Wage Programme

--- EMP_Wage_NewBatch/Program.cs
+++ EMP_Wage_NewBatch/Program.cs
@@ -10,28 +10,29 @@ namespace EMP_Wage_NewBatch
     {
         static void Main(string[] args)
         {
+            int DailyHours = 0;
+            int DH = DailyHours;
             Console.WriteLine("Welcome to Employee Wage Programme.");
             Random random = new Random();
             const int EMP_ABSENT = 0, EMP_PRESENT = 1, EMP_PARTTIME = 2;
             int empinput = random.Next(0,3);
             int WageperHour = 20;
-            int DailyHours;
             int DailyWage;
             switch(empinput)
             {
                 case EMP_PRESENT:
                 Console.WriteLine("Employee is Full-time Present");
-                DailyHours = 8;
+                DH = 8;
                 break;
 
                 case EMP_PARTTIME:           
                 Console.WriteLine("Employee is Part-time Present");
-                DailyHours = 4;
+                DH = 4;
                 break;
 
                 case EMP_ABSENT:
                 Console.WriteLine("Employee is Absent");
-                DailyHours = 0;
+                DH = 0;
                 break;
 
                 default:
@@ -39,7 +40,7 @@ namespace EMP_Wage_NewBatch
                 break;
             }
 
-            DailyWage = DailyHours * WageperHour;
+            DailyWage = DH * WageperHour;
             Console.WriteLine("Daily Wage of Employee is {0}", DailyWage);
             Console.ReadLine();
 
